73a430459434164504829565a07102969fbfdfe2
vladisdead <49247470+vladisdead@users.noreply.github.com>
2021-07-06 12:48:00 +0300

    Update config.go

--- cfg/config.go
+++ cfg/config.go
@@ -3,7 +3,7 @@ package cfg
 func New() (*CFG, error) {
 	cfg := CFG{
 		Storage: Storage{
-			Connstring: "user=test password=qwe host=192.168.20.177 port=5432 database=cache sslmode=disable",
+			Connstring: "user=test password=qwe host= port=5432 database=cache sslmode=disable",
 		},
 	}
 	return &cfg, nil
